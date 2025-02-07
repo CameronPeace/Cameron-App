110e60bbfb72c9a8069196d919c5b39013a417c3
Cameron Peace <cameronpeadev@gmail.com>
2025-02-07 14:40:08 -0500

    Added Test page

---
+++ resources/js/Components/TestComponent.jsx
@@ -0,0 +1,11 @@
+// import React, { useState, useEffect, useRef } from "react";
+import React from "react";
+
+export const TestComponent = () => {
+
+    return (
+        <div>
+           <h2>Test Component</h2>
+        </div>
+    );
+};
